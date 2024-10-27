195eff9b84c2c333fec753cecb75913be5dc57ed
ricathsb <ricat1907111@gmail.com>
2024-10-27 19:48:54 +0700

    add list item,adapter,and detail activity

--- app/src/main/java/com/mightysana/utspmo/MainActivity.kt
+++ app/src/main/java/com/mightysana/utspmo/MainActivity.kt
@@ -1,20 +1,25 @@
 package com.mightysana.utspmo
-
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        val itemList = listOf(
+            Item("Title 1", "Description 1"),
+            Item("Title 2", "Description 2"),
+            Item("Title 3", "Description 3")
+        )
+
+        val adapter = ItemAdapter(this, itemList)
+        recyclerView.adapter = adapter
     }
 }
